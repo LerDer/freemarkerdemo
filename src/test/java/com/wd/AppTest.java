5cb680bb10b237ff94c8cde2b12b59b0571efc14
lww <435971036@qq.com>
2025-07-15 00:16:58 +0800

    init

---
+++ src/test/java/com/wd/AppTest.java
@@ -0,0 +1,68 @@
+package com.wd;
+
+import com.alibaba.fastjson.JSONObject;
+import freemarker.ext.beans.BeansWrapper;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.Version;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
+
+	public void testdo() throws Exception {
+		Configuration cfg = new Configuration(new Version("2.3.28"));
+		cfg.setSharedVariable("templates", BeansWrapper.getDefaultInstance().getStaticModels());
+		cfg.setClassForTemplateLoading(AppTest.class, "/templates");
+		try (Writer out = new OutputStreamWriter(new FileOutputStream("demo.java"),
+				StandardCharsets.UTF_8)) {
+			String templatePath = "demo.java.ftl";
+			Template template = cfg.getTemplate(templatePath);
+			URL resource = AppTest.class.getClassLoader().getResource("user.json");
+			try (InputStream inputStream = resource.openStream()) {
+				JSONObject jsonObject = JSONObject.parseObject(new String(inputStream.readAllBytes()));
+				template.process(jsonObject, out);
+			} catch (Exception ignored) {
+			}
+			// 输出结果
+			String result = out.toString();
+			System.out.println(result);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
